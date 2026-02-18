c69f6b3299f3864ea3e65ea62df24ce3a55a3527
NehaM_333 <nehaamd333@gmail.com>
2026-02-18 22:35:22 +0530

    Updated the stack

--- stack/stack.java
+++ stack/stack.java
@@ -83,7 +83,7 @@ public class stack {
         }
     }
 
-    public static void pushBottom(stackA s, int data){
+    public static void pushBottom(Stack <Integer> s, int data){
         if(s.isEmpty()){
             s.push(data);
             return;
@@ -94,22 +94,84 @@ public class stack {
         s.push(top);
     }
 
+    public static String reverce(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }  
+
+        StringBuilder result = new StringBuilder();
+        while(!s.isEmpty()){
+            char curr = s.pop();    
+            result.append(curr);
+        }
+        return result.toString();
+    }
+
+    public static void reverceStack(Stack <Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        
+        int top = s.pop();
+        reverceStack(s);
+        pushBottom(s, top);
+    }
+
+    public static void stockSpan(int stock[], int span[]){
+        Stack <Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for (int i = 1; i < stock.length; i++) {
+            int curr = stock[i];
+
+            while(!s.isEmpty() && curr >= stock[s.peek()]){
+                s.pop();
+            }
+
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }else{
+                int prev = s.peek();
+                span[i] = i - prev;
+            }
+            s.push(i);
+        }
+    }
+
     public static void main(String[] args){
         stackA s = new stackA();
 
         // stackB s = new stackB();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
         // while(!s.isEmptyA()){
         //     System.out.println(s.peekA());
         //     s.popA();
         // }
-        pushBottom(s, 4);
-        while(!s.isEmpty()){
-            System.out.println(s.peek());
-            s.pop();
+
+        //pushBottom(s, 4);
+        // while(!s.isEmpty()){
+        //     System.out.println(s.pop());
+        // }
+
+        // String str = "abc";
+        // String result = reverce(str);
+        // System.out.println(result);
+
+        int[] stocks = {100, 80, 60, 70, 60, 85, 100};
+        int span[] = new int[stocks.length];
+        stockSpan(stocks, span);
+
+        for(int i=0; i<span.length; i++){
+            System.out.print(span[i]+ " ");
         }
+        System.out.println();
     }   
 }
